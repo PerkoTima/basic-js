c6afa85420f9632c60bc9ced34d85febdb0e0471
PerkoTima <perko.t@mail.ru>
2020-09-29 16:24:32 +0300

    feat: completed dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+	let dreamTeam = ''
+	let new_arr = []
+	if(Array.isArray(arr)){
+		new_arr = arr.filter(elem => typeof elem  === 'string').map(elem => elem.toUpperCase().trim()).sort()
+		for(let i = 0; i < new_arr.length; i++){
+			dreamTeam += new_arr[i][0]
+		}
+		return dreamTeam
+	}else{
+		return false
+	}
+	
+		
 };
