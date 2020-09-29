0754601c49fd03577f355b916416062a7e467943
PerkoTima <perko.t@mail.ru>
2020-09-29 23:28:57 +0300

    feat: not completed transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+	let new_arr = []
+	if(Array.isArray(arr)){
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] === '--discard-next') {
+				if ((i += 2) <= arr.length) {
+					i += 2
+				}
+			}else if (arr[i] === '--discard-prev'){
+				new_arr.pop()
+			}else if (arr[i] === '--double-next' ){
+				if (arr[i + 1] !== undefined) {
+					new_arr.push(arr[i + 1])
+				}
+			}else if (arr[i] === '--double-prev'){
+				if (arr[i - 1] !== undefined) {
+					new_arr.push(new_arr[new_arr.length - 1])
+				}
+			}else{
+				new_arr.push(arr[i])
+			}
+		}
+		return new_arr
+	}else{
+		throw new Error
+	}
 };
